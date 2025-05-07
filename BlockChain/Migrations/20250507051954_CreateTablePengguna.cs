df7b6ef1660565cd8f3d22cc9a18b80a52f1097f
AzureAD\DewaGdeMahendraPutra <d.putra@lakeba.com>
2025-05-07 13:21:04 +0800

    Add Pengguna entity and update database context
    This commit introduces a new `Pengguna` entity representing users in the system with properties such as `Id`, `Nama`, `Username`, `NamaToko`, `Password`, `Email`, and `Role`. The `AccountController` has been updated to register new users by adding `Pengguna` objects to the database context.
    
    Additionally, the `ApplicationDbContext` now includes a `DbSet<Pengguna>` for managing these entities. The model snapshot has been modified to reflect the new schema, and a migration file `20250507051954_CreateTablePengguna` has been created to define the database table for `Pengguna`, ensuring proper schema generation.

---
+++ BlockChain/Migrations/20250507051954_CreateTablePengguna.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlockChain.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTablePengguna : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Penggunas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nama = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    NamaToko = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Penggunas", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Penggunas");
+        }
+    }
+}
